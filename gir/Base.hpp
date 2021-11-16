89f934b8da549da0521b6989df4051fb5f097133
Anatolii Sakhnik <sakhnik@gmail.com>
2021-11-16 22:12:10 +0200

    Use virtual inheritance from gir::Base

--- gir/Base.hpp
+++ gir/Base.hpp
@@ -6,13 +6,7 @@ namespace gir {
 
 namespace GObject { class Object; }
 
-template <typename T>
 class Base
-{
-};
-
-template <>
-class Base<GObject::Object>
 {
 protected:
     ::GObject *_g_obj = nullptr;
